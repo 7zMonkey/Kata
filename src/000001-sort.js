5fd9a8516abfe2babe1d174eea50eda8aac5341d
7zMonkey <7zMonkey@aliyun.com>
2023-05-22 11:52:21 +0800

    answer: 000001

---
+++ src/000001-sort.js
@@ -0,0 +1,15 @@
+export default function bubbleSort(arr) {
+  arr = JSON.parse(JSON.stringify(arr))
+  const len = arr.length;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] < arr[j]) {
+        const temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
